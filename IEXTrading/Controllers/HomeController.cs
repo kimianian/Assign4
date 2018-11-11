089694f7eb591918392571948be44d5504e3bcaf
Unknown <kimiak@mail.usf.edu>
2018-11-11 17:51:12 -0500

    Files Added Again

---
+++ IEXTrading/Controllers/HomeController.cs
@@ -0,0 +1,232 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using IEXTrading.Infrastructure.IEXTradingHandler;
+using IEXTrading.Models;
+using IEXTrading.Models.ViewModel;
+using IEXTrading.DataAccess;
+using Newtonsoft.Json;
+
+
+namespace MVCTemplate.Controllers
+{
+
+        public class HomeController : Controller
+    {
+        public ApplicationDbContext dbContext;
+
+        public HomeController(ApplicationDbContext context)
+        {
+            dbContext = context;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        /****
+         * The Symbols action calls the GetSymbols method that returns a list of Companies.
+         * This list of Companies is passed to the Symbols View.
+        ****/
+        public IActionResult Symbols()
+        {
+            //Set ViewBag variable first
+            ViewBag.dbSucessComp = 0;
+            IEXHandler webHandler = new IEXHandler();
+            List<Company> companies = webHandler.GetSymbols();
+
+            //Save comapnies in TempData
+            TempData["Companies"] = JsonConvert.SerializeObject(companies);
+
+            return View(companies);
+        }
+
+        /****
+         * The Chart action calls the GetChart method that returns 1 year's equities for the passed symbol.
+         * A ViewModel CompaniesEquities containing the list of companies, prices, volumes, avg price and volume.
+         * This ViewModel is passed to the Chart view.
+        ****/
+        public IActionResult Chart(string symbol)
+        {
+            //Set ViewBag variable first
+            ViewBag.dbSuccessChart = 0;
+            List<Equity> equities = new List<Equity>();
+           if (symbol != null)
+             {
+                IEXHandler webHandler = new IEXHandler();
+                equities = webHandler.GetChart(symbol);
+                equities = equities.OrderBy(c => c.date).ToList(); //Make sure the data is in ascending order of date.
+            }
+
+            CompaniesEquities companiesEquities = getCompaniesEquitiesModel(equities);
+
+            return View(companiesEquities);
+        }
+
+        /****
+         * The Refresh action calls the ClearTables method to delete records from a or all tables.
+         * Count of current records for each table is passed to the Refresh View.
+        ****/
+        public IActionResult Refresh(string tableToDel)
+        {
+            ClearTables(tableToDel);
+            Dictionary<string, int> tableCount = new Dictionary<string, int>();
+            tableCount.Add("Companies", dbContext.Companies.Count());
+            tableCount.Add("Charts", dbContext.Equities.Count());
+            return View(tableCount);
+        }
+
+        public IActionResult PortfolioSelection()
+        {
+            //Set ViewBag variable first
+            ViewBag.dbSucessComp = 0;
+            IEXHandler webHandler = new IEXHandler();
+            List<Company> companies = webHandler.GetSymbols();
+
+            //Save comapnies in TempData
+            TempData["Companies"] = JsonConvert.SerializeObject(companies);
+
+            List<double> STD_Values = new List<double>();
+            List<double> Avg_Values = new List<double>();
+            List<double> Avg_Volume = new List<double>();
+
+            foreach (Company company in companies)
+            {
+                List<Equity> equities = webHandler.GetEquities(company.symbol);
+     
+                  STD_Values.Add(StandardDeviation(equities.Select(e => e.high)));
+                  Avg_Values.Add(AvgFunc(equities.Select(e => e.high)));
+                  Avg_Volume.Add(equities.Average(e => e.volume)/ 1000000);
+            }
+
+            List<double> MeanToVar = new List<double>();
+
+            for (int i = 0; i < companies.Count(); i++) {
+                MeanToVar.Add(Avg_Values[i] / STD_Values[i]);
+            }
+            List<double> SortedMeanToVar = MeanToVar;
+            SortedMeanToVar.Sort();
+            List<Company> Best_Companies= new List<Company>();
+            int Temp_Counter = -1;
+
+            for (int i = companies.Count()-1; i >= companies.Count()-5; i--)
+            {
+                Temp_Counter = -1;
+                foreach (Company company in companies)
+                {
+                    Temp_Counter++;
+                    if (SortedMeanToVar[i] == MeanToVar[Temp_Counter]) {
+                        Best_Companies.Add(company);
+                    }
+                }
+            }
+            return View(Best_Companies);
+        }
+
+        public double StandardDeviation(IEnumerable<float> values)
+        {
+            double avg = values.Average();
+            return Math.Sqrt(values.Average(v => Math.Pow(v - avg, 2)));
+        }
+        public double AvgFunc(IEnumerable<float> values)
+        {
+            double avg = values.Average();
+            return avg;
+        }
+
+
+        /****
+         * Saves the Symbols in database.
+        ****/
+        public IActionResult PopulateSymbols()
+        {
+            List<Company> companies = JsonConvert.DeserializeObject<List<Company>>(TempData["Companies"].ToString());
+            foreach (Company company in companies)
+            {
+                //Database will give PK constraint violation error when trying to insert record with existing PK.
+                //So add company only if it doesnt exist, check existence using symbol (PK)
+                if (dbContext.Companies.Where(c => c.symbol.Equals(company.symbol)).Count() == 0)
+                {
+                    dbContext.Companies.Add(company);
+                }
+            }
+            dbContext.SaveChanges();
+            ViewBag.dbSuccessComp = 1;
+            return View("Symbols", companies);
+        }
+
+        /****
+         * Saves the equities in database.
+        ****/
+        public IActionResult SaveCharts(string symbol)
+        {
+            IEXHandler webHandler = new IEXHandler();
+            List<Equity> equities = webHandler.GetChart(symbol);
+            //List<Equity> equities = JsonConvert.DeserializeObject<List<Equity>>(TempData["Equities"].ToString());
+            foreach (Equity equity in equities)
+            {
+                if (dbContext.Equities.Where(c => c.date.Equals(equity.date)).Count() == 0)
+                {
+                    dbContext.Equities.Add(equity);
+                }
+            }
+
+            dbContext.SaveChanges();
+            ViewBag.dbSuccessChart = 1;
+
+            CompaniesEquities companiesEquities = getCompaniesEquitiesModel(equities);
+
+            return View("Chart", companiesEquities);
+        }
+
+        /****
+         * Deletes the records from tables.
+        ****/
+        public void ClearTables(string tableToDel)
+        {
+            if ("all".Equals(tableToDel))
+            {
+                //First remove equities and then the companies
+                dbContext.Equities.RemoveRange(dbContext.Equities);
+                dbContext.Companies.RemoveRange(dbContext.Companies);
+            }
+            else if ("Companies".Equals(tableToDel))
+            {
+                //Remove only those that don't have Equity stored in the Equitites table
+                dbContext.Companies.RemoveRange(dbContext.Companies
+                                                         .Where(c => c.Equities.Count == 0)
+                                                                      );
+            }
+            else if ("Charts".Equals(tableToDel))
+            {
+                dbContext.Equities.RemoveRange(dbContext.Equities);
+            }
+            dbContext.SaveChanges();
+        }
+
+        /****
+         * Returns the ViewModel CompaniesEquities based on the data provided.
+         ****/
+        public CompaniesEquities getCompaniesEquitiesModel(List<Equity> equities)
+        {
+            List<Company> companies = dbContext.Companies.ToList();
+
+            if (equities.Count == 0)
+            {
+                return new CompaniesEquities(companies, null, "", "", "", 0, 0);
+            }
+
+            Equity current = equities.Last();
+            string dates = string.Join(",", equities.Select(e => e.date));
+            string prices = string.Join(",", equities.Select(e => e.high));
+            string volumes = string.Join(",", equities.Select(e => e.volume / 1000000)); //Divide vol by million
+            float avgprice = equities.Average(e => e.high);
+            double avgvol = equities.Average(e => e.volume) / 1000000; //Divide volume by million
+            return new CompaniesEquities(companies, equities.Last(), dates, prices, volumes, avgprice, avgvol);
+        }
+    }
+}
